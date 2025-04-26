be73615051f49c5bc133e89733f5a0486d1ab945
Divahar Manohar <div@Mac-2252.cable.rcn.com>
2025-04-26 18:55:32 -0400

    updates in settings.py file

--- Churn/settings.py
+++ Churn/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6uy!h@2-2m7vfe(=7l5b5xlk+l+ux+co4$++wrs6^#4i8c7zk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['churn-prediction-6s4f.onrender.com', '127.0.0.1', 'localhost']
 
